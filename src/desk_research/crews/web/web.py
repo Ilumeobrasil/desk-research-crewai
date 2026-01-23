2f7fa853a8ec3e6c78d9983885f59a53a399f642
David Clipel <david.clipel@ilumeo.com.br>
2026-01-23 11:31:29 -0400

    Refactor web research agents and tasks in YAML and Python files; update task descriptions for clarity and specificity; implement new agents for URL validation and content extraction; enhance output structure for web research reports.

--- src/desk_research/crews/web/web.py
+++ src/desk_research/crews/web/web.py
@@ -1,9 +1,11 @@
 import datetime
+import json
 
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from desk_research.constants import VERBOSE_AGENTS, VERBOSE_CREW
 from desk_research.tools.research_tools import google_search_tool, web_scraper_tool, url_validator_tool
+from desk_research.utils.makelog.makeLog import make_log
 from desk_research.utils.reporting import export_report
 
 @CrewBase
@@ -11,23 +13,36 @@ class WebCrew:
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
 
-    def web_researcher(self) -> Agent:
+    @agent
+    def web_researcher_url(self) -> Agent:
         return Agent(
-            config=self.agents_config['web_researcher'],
+            config=self.agents_config['web_researcher_url'],
             tools=[
                 google_search_tool,
-                web_scraper_tool,
                 url_validator_tool
             ],
-            verbose=VERBOSE_AGENTS
+            verbose=True
         )
 
     @agent
+    def web_researcher_content(self) -> Agent:
+        return Agent(
+            config=self.agents_config['web_researcher_content'],
+            tools=[
+                web_scraper_tool,
+            ],
+            verbose=True
+        )
+
+    """ @agent
     def content_analyzer(self) -> Agent:
         return Agent(
             config=self.agents_config['content_analyzer'],
-            verbose=VERBOSE_AGENTS
-        )
+            verbose=VERBOSE_AGENTS,
+            tools=[
+                web_scraper_tool,
+            ]
+        ) """
 
     @agent
     def web_report_writer(self) -> Agent:
@@ -37,23 +52,32 @@ class WebCrew:
         )
 
     @task
-    def search_web(self) -> Task:
+    def search_web_urls(self) -> Task:
         return Task(
-            config=self.tasks_config['search_web'],
-            agent=self.web_researcher()
+            config=self.tasks_config['search_web_urls'],
+            agent=self.web_researcher_url()
         )
 
     @task
-    def analyze_content(self) -> Task:
+    def extract_web_content(self) -> Task:
         return Task(
-            config=self.tasks_config['analyze_content'],
+            config=self.tasks_config['extract_web_content'],
+            agent=self.web_researcher_content()
+        )
+
+    """ @task
+    def classify_organize_content(self) -> Task:
+        return Task(
+            config=self.tasks_config['classify_organize_content'],
+            context=[self.search_web()],
             agent=self.content_analyzer()
-        )
+        ) """
 
     @task
-    def create_web_report(self) -> Task:
+    def create_structure(self) -> Task:
         return Task(
-            config=self.tasks_config['create_web_report'],
+            config=self.tasks_config['create_structure'],
+            context=[self.extract_web_content()],
             agent=self.web_report_writer()
         )
 
@@ -71,12 +95,37 @@ def run_web_research(query: str, max_results: int = 10):
         inputs = {
             'query': query,
             'max_results': max_results,
+            'max_results_extractions': max_results + 5,
             'current_date': datetime.datetime.now().strftime('%d/%m/%Y')
         }
         
         crew = WebCrew()
+        
+        #remover
         print("RODANDO WEB")
+        start_time = time.time()
+
         result = crew.crew().kickoff(inputs=inputs)
+        
+        #remover
+        end_time = time.time()
+        execution_time = end_time - start_time
+        minutes = int(execution_time // 60)
+        seconds = int(execution_time % 60)
+        if minutes > 0:
+            print(f"TERMINOU WEB - Tempo de execução: {minutes}m {seconds}s")
+        else:
+            print(f"TERMINOU WEB - Tempo de execução: {seconds}s")
+   # Este documento NÃO contém respostas, análises ou conclusões.
+        make_log({
+            "logName": f"web_research",
+            "content": {
+                "result": result,
+                "query": query,
+                "max_results": max_results,
+                "current_date": datetime.datetime.now().strftime('%d/%m/%Y')
+            }
+        })
         
         export_report(result, query, prefix="web_report", crew_name="web")
         
